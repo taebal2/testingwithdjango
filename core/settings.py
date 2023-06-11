0328bce24684ce8e85974a3056b74c189615184a
taebal2 <rlaxodms5161@gmail.com>
2023-06-12 04:17:08 +0900

    alow host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_sk1oy#y3=g$$*q06svhnzhb^go5=@$8rn@dj0&8vcg*gla*_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.139.85.73']
 
 
 # Application definition
